4bf33bf71dc18de52f695f7a1f7de91e208e3b62
Andreas Ioannou <ioannou_andreas@hotmail.gr>
2018-10-27 18:34:40 +0100

    refactor: created presenter

---
+++ app/src/main/java/com/shapes/ui/editor/EditorPresenter.java
@@ -0,0 +1,42 @@
+package com.shapes.ui.editor;
+
+import javax.inject.Inject;
+
+/**
+ * Created by xnorcode on 27/10/2018.
+ */
+public class EditorPresenter implements EditorContract.Presenter {
+
+    private int canvaWidth;
+
+    private int canvaHeight;
+
+    private int shapeRadius;
+
+    private EditorContract.View view;
+
+    @Inject
+    public EditorPresenter() {
+    }
+
+    @Override
+    public void init(int canvaWidth, int canvaHeight) {
+        this.canvaWidth = canvaWidth;
+        this.canvaHeight = canvaHeight;
+    }
+
+    @Override
+    public void generateShape(int type) {
+
+    }
+
+    @Override
+    public void setView(EditorContract.View view) {
+        this.view = view;
+    }
+
+    @Override
+    public void dropView() {
+        view = null;
+    }
+}
