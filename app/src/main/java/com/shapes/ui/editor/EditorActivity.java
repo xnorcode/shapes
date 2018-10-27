9f3372d315fd5586c115fcb416ee62342347c34e
Andreas Ioannou <ioannou_andreas@hotmail.gr>
2018-10-27 15:12:38 +0100

    refactor: created fragment and added to activity

--- app/src/main/java/com/shapes/ui/editor/EditorActivity.java
+++ app/src/main/java/com/shapes/ui/editor/EditorActivity.java
@@ -11,5 +11,17 @@ public class EditorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+
+        // remove activity's background theme
+        getWindow().setBackgroundDrawable(null);
+
+        // Add fragment to activity
+        EditorFragment fragment = (EditorFragment) getSupportFragmentManager().findFragmentById(R.id.editor_fragment_container);
+        if (fragment == null) {
+            fragment = new EditorFragment();
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.editor_fragment_container, fragment)
+                    .commit();
+        }
     }
 }
---
+++ app/src/main/java/com/shapes/ui/editor/EditorFragment.java
@@ -0,0 +1,10 @@
+package com.shapes.ui.editor;
+
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by xnorcode on 27/10/2018.
+ */
+public class EditorFragment extends Fragment {
+}
