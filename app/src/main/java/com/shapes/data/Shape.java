701403c8e5241423f6a127a9d6499dd2e129ce03
Andreas Ioannou <ioannou_andreas@hotmail.gr>
2018-10-29 00:03:07 +0000

    refactor: override equal and hascode

--- app/src/main/java/com/shapes/data/Shape.java
+++ app/src/main/java/com/shapes/data/Shape.java
@@ -61,4 +61,30 @@ public class Shape {
     public void setViewIndex(int viewIndex) {
         this.viewIndex = viewIndex;
     }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + id;
+        result = 31 * result + type;
+        result = 31 * result + color;
+        result = 31 * result + gridIndex;
+        result = 31 * result + viewIndex;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Shape shape = (Shape) obj;
+
+        return shape.getId() == this.id
+                && shape.getType() == this.type
+                && shape.getColor() == this.color
+                && shape.getGridIndex() == this.gridIndex
+                && shape.getViewIndex() == this.viewIndex;
+    }
 }
