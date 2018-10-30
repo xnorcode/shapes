6dff918f35376f55fe175e5daec95d8658afb219
Andreas Ioannou <ioannou_andreas@hotmail.gr>
2018-10-30 15:20:31 +0000

    refactor: added adapter and presenter in fragment

--- app/src/main/java/com/shapes/ui/stats/StatsFragment.java
+++ app/src/main/java/com/shapes/ui/stats/StatsFragment.java
@@ -3,6 +3,7 @@ package com.shapes.ui.stats;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +14,8 @@ import com.shapes.R;
 import java.util.List;
 import java.util.Map;
 
+import javax.inject.Inject;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import dagger.android.support.DaggerFragment;
@@ -22,11 +25,17 @@ import dagger.android.support.DaggerFragment;
  */
 public class StatsFragment extends DaggerFragment implements StatsContract.View {
 
-    // TODO: 29/10/2018 configure stats list screen
 
     @BindView(R.id.statsRecyclerView)
     RecyclerView recyclerView;
 
+
+    @Inject
+    StatsRecyclerViewAdapter adapter;
+
+
+    @Inject
+    StatsPresenter presenter;
 
 
     @Nullable
@@ -36,16 +45,42 @@ public class StatsFragment extends DaggerFragment implements StatsContract.View 
 
         ButterKnife.bind(this, rootView);
 
+        // set layout manager
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        // bind adapter to recycler view
+        recyclerView.setAdapter(adapter);
+
         return rootView;
     }
 
     @Override
-    public void showCounts(List<Map.Entry<String, Integer>> groupedShapes) {
+    public void onStart() {
+        super.onStart();
 
+        // bind view to presenter
+        presenter.setView(this);
+
+        // initialize presenter
+        presenter.calculateStats();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // remove presenter
+        if (presenter != null) presenter.dropView();
+        presenter = null;
+    }
+
+    @Override
+    public void showCounts(List<Map.Entry<String, String>> groupedShapes) {
+        adapter.swapData(groupedShapes);
     }
 
     @Override
     public void setPresenter(StatsContract.Presenter presenter) {
-
+        // used for manual injection of presenter
     }
 }
